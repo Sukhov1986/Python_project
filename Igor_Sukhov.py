bea7ffbc68def776c02f2105ec7540f573951e98
Igor_Sukhov <mebunu94@gmail.com>
2023-12-18 13:35:08 +0300

    Homework 17.12.2023

--- Igor_Sukhov.py
+++ Igor_Sukhov.py
@@ -13,7 +13,18 @@
 # for i in lst:
 #     print(f"{i} = {tpl.count(i)}")
 
-string = input('Введите строку -> ').lower()
-vowels_1 = {'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'}
-vowels_2 = [x for x in string if x in vowels_1]
-print(f'Количество гласных равно : {len(vowels_2)}')
+# string = input('Введите строку -> ').lower()
+# vowels_1 = {'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'}
+# vowels_2 = [x for x in string if x in vowels_1]
+# print(f'Количество гласных равно : {len(vowels_2)}')
+
+dictionary_1 = {"name": "kelly", "age": 25, "salary": 8000, "city": "New York"}
+# print(dictionary_1)
+dictionary_2 = {key: dictionary_1[key] for key in dictionary_1 if key in ["name", "salary"]}
+for i in dictionary_2:
+    dictionary_1.pop(i)
+
+
+print(dictionary_1)
+print(dictionary_2)
+
